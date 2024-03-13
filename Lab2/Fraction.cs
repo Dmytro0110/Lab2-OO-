3b3a8b5a42f37b55f032d40a9dd5f1af21f22b71
Dmytro0110 <dmitroonisenko@gmail.com>
2024-03-13 21:12:10 +0200

    Добавьте файлы проекта.

---
+++ Lab2/Fraction.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new ArgumentException("Знаменник не може дорівнювати нулю!");
+            this.numerator = numerator;
+            this.denominator = denominator;
+            Simplify();
+        }
+
+        public Fraction(Fraction other)
+        {
+            this.numerator = other.numerator;
+            this.denominator = other.denominator;
+        }
+
+        private int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        private void Simplify()
+        {
+            int gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+            numerator /= gcd;
+            denominator /= gcd;
+            if (denominator < 0)
+            {
+                numerator *= -1;
+                denominator *= -1;
+            }
+        }
+
+        public Fraction Add(Fraction other)
+        {
+            int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+            int newDenominator = this.denominator * other.denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public Fraction Subtract(Fraction other)
+        {
+            int newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+            int newDenominator = this.denominator * other.denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public Fraction Multiply(Fraction other)
+        {
+            int newNumerator = this.numerator * other.numerator;
+            int newDenominator = this.denominator * other.denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public Fraction Divide(Fraction other)
+        {
+            int newNumerator = this.numerator * other.denominator;
+            int newDenominator = this.denominator * other.numerator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public Fraction Power(int exponent)
+        {
+            int newNumerator = (int)Math.Pow(this.numerator, exponent);
+            int newDenominator = (int)Math.Pow(this.denominator, exponent);
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public bool Equals(Fraction other)
+        {
+            return this.numerator == other.numerator && this.denominator == other.denominator;
+        }
+
+        public bool IsGreaterThan(Fraction other)
+        {
+            return this.numerator * other.denominator > other.numerator * this.denominator;
+        }
+
+        public bool IsLessThan(Fraction other)
+        {
+            return this.numerator * other.denominator < other.numerator * this.denominator;
+        }
+
+        public override string ToString()  
+        {
+            return numerator + "/" + denominator;
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction(7, 4);
+            Fraction fraction2 = new Fraction(1, 2);
+
+            Console.WriteLine("Дріб 1: " + fraction1);
+            Console.WriteLine("Дріб 2: " + fraction2);
+
+            Fraction sum = fraction1.Add(fraction2);
+            Console.WriteLine("Сума: " + sum);
+
+            Fraction difference = fraction1.Subtract(fraction2);
+            Console.WriteLine("Різниця: " + difference);
+
+            Fraction product = fraction1.Multiply(fraction2);
+            Console.WriteLine("Добуток: " + product);
+
+            Fraction quotient = fraction1.Divide(fraction2);
+            Console.WriteLine("Частка: " + quotient);
+
+            Fraction power = fraction1.Power(2);
+            Console.WriteLine("Дріб 1 в квадраті: " + power);
+
+            Console.WriteLine("Дріб 1 більше дробу 2? " + fraction1.IsGreaterThan(fraction2));
+            Console.WriteLine("Дріб 1 менший дробу 2? " + fraction1.IsLessThan(fraction2));
+            Console.WriteLine("Дріб 1 рівний дробу 2? " + fraction1.Equals(fraction2));
+        }
+    }
+}
